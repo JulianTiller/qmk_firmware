fbf28f4110d8d22b616f3e866ef1c7926ef45eb3
JulianTiller <juls.pfl@gmail.com>
2024-07-28 07:34:36 +0200

    removed NUM layer, moved qmk stuff to own layer, changed arrow keys to vim layout, moved pg-keys, moved meta key, added Fkeys, moved boot keys to sys layer

--- keyboards/crkbd/jp_custom/keymaps/Juls/keymap.c
+++ keyboards/crkbd/jp_custom/keymaps/Juls/keymap.c
@@ -31,8 +31,9 @@ enum corne_layers {
     _QWERTY,//L1
     _COL,   //L2
     _SYM,   //L3
-    _NUM,   //L4
-    _SYS,   //L5
+    //_NUM,   //L4
+    _SYS,   //L4,5
+    _QMK,   //L5,6
     __EMPTY
 };
 
@@ -42,34 +43,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [0] = LAYOUT_split_3x6_3(   KC_TAB,  DE_Q, DE_W, DE_E, DE_R, DE_T,      DE_Z, DE_U, DE_I,    DE_O,    DE_P,    KC_BSPC,
                                     KC_LSFT, DE_A, DE_S, DE_D, DE_F, DE_G,      DE_H, DE_J, DE_K,    DE_L,    DE_HASH, KC_RSFT,
                                     KC_LCTL, DE_Y, DE_X, DE_C, DE_V, DE_B,      DE_N, DE_M, DE_COMM, DE_DOT,  DE_MINS, KC_ESC,
-                                                    KC_LALT, MO(3),KC_SPC,      KC_ENT, MO(3), KC_RALT
+                                                   KC_LALT, MO(3), KC_SPC,      KC_ENT, MO(3), KC_RALT
                                 ),
         [1] = LAYOUT_split_3x6_3(   KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T,      KC_Y, KC_U, KC_I,    KC_O,    KC_P,    KC_BSPC,
                                     KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,      KC_H, KC_J, KC_K,    KC_L,    KC_HASH, KC_LGUI,
                                     KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,      KC_N, KC_M, KC_COMM, KC_DOT,  KC_MINS, KC_ESC,
-                                    TT(4),   TT(3),  KC_SPC,                    KC_ENT, TT(5), KC_RALT
+                                                   KC_LALT, MO(3), KC_SPC,      KC_ENT, MO(3), KC_RALT
                                 ),
         [2] = LAYOUT_split_3x6_3(   KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T,      KC_Y, KC_U, KC_I,    KC_O,    KC_P,    KC_BSPC,
                                     KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,      KC_H, KC_J, KC_K,    KC_L,    KC_HASH, KC_LGUI,
                                     KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,      KC_N, KC_M, KC_COMM, KC_DOT,  KC_MINS, KC_ESC,
-                                    TT(4),   TT(3),  KC_SPC,                    KC_ENT, TT(5), KC_RALT
+                                                   KC_LALT, MO(3), KC_SPC,      KC_ENT, MO(3), KC_RALT
                                 ),
 
-        /*[3] = LAYOUT_split_3x6_3(   DE_CIRC,  DE_1,    DE_2,    DE_3,    DE_4,    DE_5,        DE_6,    DE_7,    DE_8,    DE_9,    DE_0,    KC_BSPC,
-                                    KC_LSFT,  DE_SLSH, DE_LCBR, DE_LBRC, DE_LPRN, DE_LABK,     DE_RABK, DE_RPRN, DE_RBRC, DE_RCBR, DE_BSLS, KC_NO,
-                                    KC_CAPS,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                    KC_NO,    KC_TRNS, KC_NO,                                  KC_NO,   KC_NO,   KC_RALT
-                                ),*/
         [3] = LAYOUT_split_3x6_3(   DE_CIRC,  DE_1,    DE_2,    DE_3,    DE_4,    DE_5,        DE_6,    DE_7,    DE_8,    DE_9,    DE_0,    DE_SS,
-                                    KC_LSFT,  DE_SLSH, DE_LCBR, DE_LBRC, DE_LPRN, DE_LABK,     KC_PGUP, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_RSFT,
-                                    KC_LCTL,  DE_BSLS, DE_RCBR, DE_RBRC, DE_RPRN, DE_RABK,     KC_PGDN, TO(4),   TO(5),   KC_NO,   KC_NO,   KC_LGUI,
-                                    KC_CAPS,  KC_TRNS, KC_BTN1,                                KC_BTN2,   KC_TRNS, MOUSEJIGGLERMACRO
+                                    KC_LSFT,  DE_SLSH, DE_LCBR, DE_LBRC, DE_LPRN, DE_LABK,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, DE_PLUS, KC_RSFT,
+                                    KC_LCTL,  DE_BSLS, DE_RCBR, DE_RBRC, DE_RPRN, DE_RABK,     KC_PGDN, KC_PGUP, TO(4),   TO(5),   DE_PIPE, KC_NO,
+                                    KC_CAPS,  KC_TRNS, KC_LGUI,                                KC_LGUI, KC_TRNS, MOUSEJIGGLERMACRO
                                 ),
 
-        [4] = LAYOUT_split_3x6_3(   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,           KC_NUM, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_PMNS,
-                                    KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,           KC_NO,  KC_P4, KC_P5, KC_P6, KC_PAST, KC_PSLS,
-                                    KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,           KC_P0,  KC_P1, KC_P2, KC_P3, KC_NO,   KC_EQL,
-                                    KC_NO,   KC_NO, TG(4),                                TG(4),  KC_NO , KC_NO
+        //[4] = LAYOUT_split_3x6_3(   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,           KC_NUM, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_PMNS,
+        //                            KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,           KC_NO,  KC_P4, KC_P5, KC_P6, KC_PAST, KC_PSLS,
+        //                            KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,           KC_P0,  KC_P1, KC_P2, KC_P3, KC_NO,   KC_EQL,
+        //                            KC_NO,   KC_NO, TG(4),                                TG(4),  KC_NO , KC_NO
+        //                        ),
+
+        [4] = LAYOUT_split_3x6_3(   KC_F1,   KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,           KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
+                                    KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,           KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,  KC_NO,
+                                    QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,           KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,  QK_BOOT,
+                                    KC_NO,   KC_NO, TG(4),                                TG(4), KC_NO, KC_NO
                                 ),
 
         [5] = LAYOUT_split_3x6_3(   QK_BOOT,    KC_NO,   KC_NO,   KC_NO,   RGB_TOG, BL_BRTG,          KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,   DF(0),
@@ -77,7 +79,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     RGB_RMOD,   RGB_SPD, RGB_VAD, RGB_HUD, RGB_SAD, BL_DOWN,          KC_PGDN, KC_NO,   KC_NO,   KC_NO, KC_NO,   DF(2),
                                     KC_NO,      KC_NO, TG(5),                                         TG(5),   KC_NO,   KC_NO
                                 )
+
 /*
+
+         [3] = LAYOUT_split_3x6_3(   DE_CIRC,  DE_1,    DE_2,    DE_3,    DE_4,    DE_5,        DE_6,    DE_7,    DE_8,    DE_9,    DE_0,    KC_BSPC,
+                                    KC_LSFT,  DE_SLSH, DE_LCBR, DE_LBRC, DE_LPRN, DE_LABK,     DE_RABK, DE_RPRN, DE_RBRC, DE_RCBR, DE_BSLS, KC_NO,
+                                    KC_CAPS,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                                    KC_NO,    KC_TRNS, KC_NO,                                  KC_NO,   KC_NO,   KC_RALT
+                                ),
+
         [1] = LAYOUT_split_3x6_3(   KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,       KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
                                     KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,      KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_LGUI,
                                     KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,      KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ESC,
@@ -276,11 +286,14 @@ static void print_status_narrow(void) {
         case _SYM:
             oled_write("SYM  ", false);
             break;
-        case _NUM:
-            oled_write("NUM  ", false);
-            break;
+        //case _NUM:
+        //    oled_write("NUM  ", false);
+        //    break;
         case _SYS:
             oled_write("SYS  ", false);
+            break;
+        case _QMK:
+            oled_write("QMK  ", false);
             break;
         default:
             oled_write("Undef", false);
